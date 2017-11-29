e19a26de72a610215a5ada530b0a6926a906c30d
Erik Edlund <erik_edlund@hotmail.se>
2017-11-29 09:41:47 +0100

    Finished the method add and created tests for it

---
+++ src/main/java/com/example/maven/git_with_branch_calculator/Calculator.java
@@ -0,0 +1,21 @@
+package com.example.maven.git_with_branch_calculator;
+
+public class Calculator {
+	public int add(int firstNumber,int secondNumber) {
+		return firstNumber + secondNumber;
+	}
+	
+	//jag la till det här själv
+	public int subtract(int firstNumber,int secondNumber) {
+		return firstNumber - secondNumber;
+	}
+	
+	public int multiply(int firstNumber,int secondNumber) {
+		return firstNumber * secondNumber;
+	}
+	
+	public int divide(int firstNumber,int secondNumber) {
+		return firstNumber / secondNumber;
+	}
+
+}

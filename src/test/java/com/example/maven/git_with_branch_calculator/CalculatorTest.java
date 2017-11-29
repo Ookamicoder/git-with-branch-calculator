d69ab708e91cc3d3ed5ed51dbe98c2ec96b852cd
Erik Edlund <erik_edlund@hotmail.se>
2017-11-29 10:12:17 +0100

    I added the rest of the methods in Calculator.java

--- src/test/java/com/example/maven/git_with_branch_calculator/CalculatorTest.java
+++ src/test/java/com/example/maven/git_with_branch_calculator/CalculatorTest.java
@@ -27,6 +27,54 @@ public class CalculatorTest {
 		}
 	}
 	
+	@Test
+	public void testSubtract() {
+		Random random = new Random();
+		int firstNumber = 0;
+		int secondNumber = 0;
+		int result = 0;
+		
+		for(int i = 0; i < 10; i++) {
+		firstNumber = random.nextInt(100);
+		secondNumber = random.nextInt(100);
+		result = firstNumber - secondNumber;
+		LOG.info("Testing the method subtract with: "+firstNumber+" and "+secondNumber);
+		assertEquals(calc.subtract(firstNumber,secondNumber),result);
+		}
+	}
+	
+	@Test
+	public void testMultiply() {
+		Random random = new Random();
+		int firstNumber = 0;
+		int secondNumber = 0;
+		int result = 0;
+		
+		for(int i = 0; i < 10; i++) {
+		firstNumber = random.nextInt(100);
+		secondNumber = random.nextInt(100);
+		result = firstNumber * secondNumber;
+		LOG.info("Testing the method multiply with: "+firstNumber+" and "+secondNumber);
+		assertEquals(calc.multiply(firstNumber,secondNumber),result);
+		}
+	}
+	
+	@Test
+	public void testDivide() {
+		Random random = new Random();
+		int firstNumber = 0;
+		int secondNumber = 0;
+		int result = 0;
+		
+		for(int i = 0; i < 10; i++) {
+		firstNumber = random.nextInt(100);
+		secondNumber = random.nextInt(100);
+		result = firstNumber / secondNumber;
+		LOG.info("Testing the method divide with: "+firstNumber+" and "+secondNumber);
+		assertEquals(calc.divide(firstNumber,secondNumber),result);
+		}
+	}
+	
 
 
 }
